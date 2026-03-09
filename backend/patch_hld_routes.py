db9129f217508353ad6c52c84eff72a77027c210
Ido Katz <13540648+idokatz86@users.noreply.github.com>
2026-03-09 16:52:47 +0200

    chore: cleanup test artifacts and script files

--- backend/patch_hld_routes.py
+++
@@ -1,8 +0,0 @@
-with open("routers/hld_routes.py", "r") as f:
-    code = f.read()
-
-code = code.replace("await asyncio.to_thread(\\n            diagrams_compat.generate_hld,", "await diagrams_compat.generate_hld(")
-
-with open("routers/hld_routes.py", "w") as f:
-    f.write(code)
-print("hld routes patched")
